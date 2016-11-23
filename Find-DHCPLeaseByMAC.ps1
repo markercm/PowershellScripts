4bab625a2c6c5de8142f34a89acdfeb04c936d72
Curt Marker <marker.43@osu.edu>
2016-11-23 08:57:58 -0500

    Removed specific servers.  Generalized.

---
+++ Find-DHCPLeaseByMAC.ps1
@@ -0,0 +1,15 @@
+﻿#Script used to search for Reservations across all scopes on a DHCP server
+
+#Set DHCP Server address
+$dhcpServer = ""
+
+
+#Set the target MAC that you are trying to find.
+#Format like AA-BB-11-22-CC-DD
+$targetMAC = ""
+
+$scopes=$(Get-DhcpServerv4Scope –ComputerName $dhcpServer)
+Foreach ($scope in $scopes)
+    {
+    Get-DhcpServerv4Lease -ComputerName $dhcpServer -ScopeID $scope.scopeID -ClientId $targetMAC 2> $null
+    }
